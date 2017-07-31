e5cf7912d1ecf721717fcef694c690d1f112923e
GroupPluginer <4gamesl7o@gmail.com>
2017-07-31 03:44:51 +0300

    Create Main.php

---
+++ src/ct/Main.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace cf;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat as TF;
+
+ class Main extends PluginBase{
+ 
+  public function onEnable(){
+   $this->getServer()->getLogger()->info(TF::GREEN."Config has been enabled");
+    @mkdir($this->getDataFolder());
+    $message = [
+     'msg' => 'Welcome',
+   ];
+   $cfg = new Config($this->getDataFolder() ."config.yml", Config::YAML,$message);
+    }
+    
+    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+     switch($cmd->getName());
+      case 'msg':
+       $msg = $this->getConfig()->get("msg");
+       $sender->sendMessage($msg);
+       }
+      }
+     }
+      
+     
+    
